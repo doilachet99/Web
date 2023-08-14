0ad1f2cebc1e067fbf7cf7bbfb7441eb0968ec57
doilachet99 <doilachet99@gmail.com>
2023-08-14 15:24:34 +0700

    repair get db and find way connect NAS

--- Web/Controllers/NewsController.cs
+++ Web/Controllers/NewsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Web.Models;
+using Web.Views.News;
+
 namespace Web.Controllers
 {
     public class NewsController : Controller
@@ -12,70 +14,17 @@ namespace Web.Controllers
         // GET: News
         public ActionResult Index()
         {
-
-            /*
             var ParentId = Guid.Parse("3EC92834-25A8-4C86-97C8-33E8FDE88A0F");
-
             var categories = db.Categories
                 .Where(c => c.ParentId == ParentId)
-                .Select(c => new
+                .Select(c => new CategoryViewModel
                 {
-                    c.Id,
-                    c.Name,
+                    Id = c.Id,
+                    Name = c.Name,
                     Total = db.News_Category
                     .Where(n => n.CategoryId == c.Id).Count()
                 }).ToList();
-            */
-
-
-            var targetParentId = Guid.Parse("3EC92834-25A8-4C86-97C8-33E8FDE88A0F");
-
-            var categories = db.Categories
-                            .Where(c => c.ParentId == targetParentId)
-                            .Select(c => new
-                            {
-                                Id = c.Id,
-                                Name = c.Name,
-                                Total = db.News_Category.Count(n => n.CategoryId == c.Id)
-                            })
-                            .ToList();
-
-            ViewBag.Categories = categories;
-            return View();
-
-
-
-            /*var targetParentId = Guid.Parse("3EC92834-25A8-4C86-97C8-33E8FDE88A0F");
-            var categories = db.Categories
-                            .Where(c => c.ParentId == targetParentId)
-                            .Select(c => new
-                            {
-                                c.Id,
-                                c.Name,
-                                Total = db.News_Category.Count(n => n.CategoryId == c.Id)
-                            })
-                            .ToList();
-
-            ViewBag.Categories = categories;
-            return View();*/
-            /*
-                        var targetParentId = Guid.Parse("3EC92834-25A8-4C86-97C8-33E8FDE88A0F");
-
-                        var categories = db.Categories
-                                        .Where(c => c.ParentId == targetParentId)
-                                        .Select(c => new
-                                        {
-                                            Id = c.Id,
-                                            Name = c.Name,
-                                            Total = db.News_Category.Count(n => n.CategoryId == c.Id)
-                                        })
-                                        .ToList();
-            */
-
+            return View(categories);
         }
-
-
-
-
     }
 }
---
+++ Web/Views/News/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Views.News
+{
+    public class CategoryViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Total { get; set; }
+    }
+}
